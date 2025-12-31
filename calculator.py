bea12667243f11c6200972f3948deb57b11c81bf
Gunnar Babicz <gunnarbabicz@yahoo.com>
2025-12-31 15:09:28 -0700

    converted calculator to accept an array of ranges and return a dictionary

--- calculator.py
+++ calculator.py
@@ -3,16 +3,16 @@ from py_ballisticcalc import *
 
 
 # given these parameters, the adjustment in mils required for the round is provided
-def calculate_drop(sight_height: float, muzzle_velocity: float, coefficient: float, zero_distance: int, shot_distance: float):
+def calculate_mil_adjustments(sight_height: float, muzzle_velocity: float, coefficient: float, zero_distance: int, distances: list[float]):
     # Changing to metric units to match Tarkov
     PreferredUnits.distance = Distance.Meter
     PreferredUnits.adjustment = Angular.Mil
 
     #Setting units
-    sight_height = Distance.Millimeter(sight_height)
+    sight_height = Unit.Millimeter(sight_height)
     muzzle_velocity = Unit.MPS(muzzle_velocity)
-    zero_distance = Unit.Meter(zero_distance)
-    shot_distance = Unit.Meter(shot_distance)
+    zero_distance = Distance.Meter(zero_distance)
+    max_distance = Distance.Meter(max(distances))
 
     # Setting a ammo type for 5.56 FMJ. This will hopefully be done with an API in the future.
     # 
@@ -21,8 +21,9 @@ def calculate_drop(sight_height: float, muzzle_velocity: float, coefficient: flo
         mv=muzzle_velocity
     )
 
-    # Creating the adar as a weapon to insert the sight height
+    # Creating the weapon to insert the sight height
     used_weapon = Weapon(sight_height=sight_height)
+
 
     # Setting the zero distance to 200 meters with shot
     calc = Calculator()
@@ -31,24 +32,43 @@ def calculate_drop(sight_height: float, muzzle_velocity: float, coefficient: flo
 
     # Setting the trajectory range an extra step so there are points on both sides to calculate
     step = Distance.Meter(10)
-    trajectory_range = shot_distance + step
+    trajectory_range = max_distance + step
 
 
     # Creating the shot distance required and running the "fire" calculation"
     hit = calc.fire(shot, trajectory_range=trajectory_range, trajectory_step=Distance.Meter(10), dense_output=True)
-    adjustment = hit.get_at("slant_distance", shot_distance)
 
+
+    # Creating a dictionary for the adjustments at each range
+    adjustment_dictionary: dict[float, float] = {}
+
+    for i in distances:
+        # Calculating the adjustment needed at this distance
+        distance = Distance.Meter(i)
+        adjustment = hit.get_at("slant_distance", distance)
+
+        # Adjusting to a float with string replacement
+        mils = float(str(adjustment.drop_angle << Angular.Mil).replace("mil", ""))
+
+        # Adding to the dictionary
+        adjustment_dictionary[i] = (mils)
     # Printing the raw result for testing purposes
-    print(f"Hold at {shot_distance}: {adjustment.drop_angle << Angular.Mil}")
+    return adjustment_dictionary
 
 
 
+# values for testing purposes
 # ballistic coefficient = 0.252
 # velocity = 904.6
 # sight height = 68.58
 # zero distance = 200
 
+distances = [50,100,150,200,250,300,350,400,450,500]
 
-calculate_drop(68.58, 904.6, 0.252, 200, 100)
+test = calculate_mil_adjustments(68.58, 904.6, 0.252, 200, distances)
+
+print(test)
+
+
 
 
